bbbb15d6fae256fa0e9472a0c795ab25b26e55e6
Alexey <alex@oblat.lv>
2025-11-09 22:01:32 +0200

    gsfaas: test main.go

--- gsfaas/main.go
+++ gsfaas/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"github.com/akyaiy/GSfass/core/config"
+)
+
+func main() {
+	var env struct {
+		Port string `mapstructure:"PORT"`
+		Host string `mapstructure:"HOST"`
+	}
+	if err := config.Read().Environment().SetBy(&env).SetDefaults(map[string]string{
+		"PORT": "8080",
+	}).End(); err != nil {
+		panic(err)
+	}
+
+	println("Host:", env.Host)
+	println("Port:", env.Port)
+}
